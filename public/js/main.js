50a2611cb3c3281533911f14ebcc29a1d1609da9
RooDie10 <hudoyrusskiyy123@gmail.com>
2024-02-14 14:35:29 +0300

    feat: started working on levels page

--- public/js/main.js
+++ public/js/main.js
@@ -6,39 +6,35 @@ const closeModal = (id) => {
 }
 
 const handleForm = (form) => {
-  form.addEventListener('htmx:after-request', formHandler = (event) => {
-    const response = JSON.parse(event.detail.xhr.response)
-    const messageField = form.querySelector('.message')
-    const inputs = form.querySelectorAll('input')
-    inputs.forEach((input) => input.classList.remove('input-error'))
-    messageField.innerHTML = ''
-    if (response.errors.length > 0) {
-      response.errors.forEach((error) => {
-        const field = form.querySelector(`[name=${error.field}]`)
-        field.classList.toggle('input-error')
-        let errorField = document.createElement('p')
-        errorField.innerHTML = error.message
-        form.querySelector('.message').append(errorField)
-      })
-    } else {
-      form.reset()
-      closeModal(form.parentElement.id)
-    }
-  })
+  form.addEventListener(
+    'htmx:after-request',
+    (formHandler = (event) => {
+      const response = JSON.parse(event.detail.xhr.response)
+      const messageField = form.querySelector('.message')
+      const inputs = form.querySelectorAll('input')
+      inputs.forEach((input) => input.classList.remove('input-error'))
+      messageField.innerHTML = ''
+      if (response.errors.length > 0) {
+        response.errors.forEach((error) => {
+          const field = form.querySelector(`[name=${error.field}]`)
+          field.classList.toggle('input-error')
+          let errorField = document.createElement('p')
+          errorField.innerHTML = error.message
+          form.querySelector('.message').append(errorField)
+        })
+      } else {
+        form.reset()
+        closeModal(form.parentElement.id)
+      }
+    })
+  )
 }
 
-const header = document.querySelector('header')
-const obsElement = document.querySelector('.int')
-const scrollWatcher = document.createElement('div')
+const disableScroll = () => {
+  document.body.style.overflowY = 'hidden'
+}
 
-scrollWatcher.setAttribute('data-scroll-watcher', '')
+const enableScroll = () => {
+  document.body.style.overflowY = ''
+}
 
-obsElement.before(scrollWatcher)
-
-const navObserver = new IntersectionObserver((e) => {
-  console.log(e);
-  header.classList.toggle('intersect', !e[0].isIntersecting)
-  header.classList.toggle('!bg-bgColor', !e[0].isIntersecting)
-})
-
-navObserver.observe(scrollWatcher)
