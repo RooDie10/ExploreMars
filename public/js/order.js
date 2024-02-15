131e92c98c2571560b3442c38eae002fbffcfc32
RooDie10 <hudoyrusskiyy123@gmail.com>
2024-02-15 15:41:54 +0300

    order page is done

--- public/js/order.js
+++ public/js/order.js
@@ -1,15 +1,23 @@
 const cardInput = document.querySelector('[name=card]')
-const cardPattern = '0000 / 0000 / 0000 / 0000'
+const cardPattern = '0 0 0 0 / 0 0 0 0 / 0 0 0 0 / 0 0 0 0'
 const cardMask = IMask(cardInput, {
   mask: cardPattern,
   lazy: false,
-  placeholderChar: ' '
+  placeholderChar: ' 0 '
 })
 
 const dateInput = document.querySelector('[name=date]')
-const datePattern = '00 / 00'
-const dateMask = IMask(cardInput, {
+const datePattern = '0 0 / 0 0'
+const dateMask = IMask(dateInput, {
   mask: datePattern,
   lazy: false,
-  placeholderChar: ' '
+  placeholderChar: ' 0 '
+})
+
+const securityInput = document.querySelector('[name=security]')
+const securityPattern = '0 0 0'
+const securityMask = IMask(securityInput, {
+  mask: securityPattern,
+  lazy: false,
+  placeholderChar: ' 0 '
 })
